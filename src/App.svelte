8872593b588fc35bbe60c4062b9546b36d2ff873
megz15 <meghraj.g16@gmail.com>
2023-04-13 12:06:11 +0530

    Added tonejs, basic notes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script>
+  import * as Tone from 'tone';
+  const synth = new Tone.PolySynth(Tone.Synth).toDestination();
   
+  function play(n) {
+    synth.triggerAttackRelease(n, '8n');
+  }
+  
+  const notes = {'B3':'Sa','C#4':'Re','D#4':'Ga','E4':'Ma','F#4':'Pa','G#4':'Dha','A#4':'Ni'}
+
 </script>
 
 <main>
   <h1>NaadGen</h1>
+  {#each Object.keys(notes) as n}
+    <button on:click={_ => play(n)}>{notes[n]}</button>
+  {/each}
 </main>
 
 <style>
