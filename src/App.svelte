24329c22b3d5a46f365dc4c969d41e9db1bebaa6
megz15 <meghraj.g16@gmail.com>
2023-04-13 14:51:24 +0530

    Vibhaagas followed, need to fix naad width

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
   import * as Tone from 'tone';
   const synth = new Tone.PolySynth(Tone.Synth).toDestination();
   
+  const taal = {
+    'Jhaptaal': [2,5,7,10]
+  }
   const notes = {'B3':'Sa','C#4':'Re','D#4':'Ga','E4':'Ma','F#4':'Pa','G#4':'Dha','A#4':'Ni'}
   let composition = []
 
@@ -16,7 +19,7 @@
 
   function playcomp() {
     composition.forEach((n,i) => {
-      play(n, Tone.now() + i/4, (i%4==0)?5:1)
+      play(n, Tone.now() + i/4, (taal['Jhaptaal'].includes(i%10) || i%10==0)?5:1)
     });
 
     // new Tone.Sequence((_, n) => {
@@ -46,12 +49,13 @@
         {/each}
     </tr> -->
     {#each composition as n, i}
-      {#if (i%10==0)}
+      {#if (i%taal['Jhaptaal'].at(-1)==0)}
         <tr></tr>
       {/if}
       <td>
-        {#if ((i%5==0 || i%7==0) && i!=0 && i%10!=0)}
-          <note>|</note><note>{n}</note>
+        <!-- {#if ((i%5==0 || i%7==0) && i!=0 && i%10!=0)} -->
+        {#if (taal['Jhaptaal'].slice(0,-1).includes(i%10))}
+          <vibhaag /><note>{n}</note>
         {:else}
           <note>{n}</note>
         {/if}
@@ -73,5 +77,21 @@
 
   table {
     border: 1px solid red;
+  }
+
+  td {
+    padding: 10px;
+  }
+
+  vibhaag {
+    border: 1px solid transparent;
+    padding: 13px 0.01px;
+    position: relative;
+    left: -15%;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #000000;
+    transition: border-color 0.25s;
   }
 </style>
